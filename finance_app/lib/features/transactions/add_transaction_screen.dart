ead6e199fdbade4fff483449f063abc82a26ced2
TheDayDreamer17 <vismaythaker008@gmail.com>
2026-06-07 19:53:57 +0530

    fix(category): import shared_preferences in add_transaction_screen

--- finance_app/lib/features/transactions/add_transaction_screen.dart
+++ finance_app/lib/features/transactions/add_transaction_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
 import '../../core/db/database_helper.dart';
 import '../../core/models/models.dart';
