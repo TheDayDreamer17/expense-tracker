ed5b2801123b57161e4d8a072ff11e99a9b48b0a
TheDayDreamer17 <vismaythaker008@gmail.com>
2026-09-06 20:06:47 +0530

    feat(branding): update app id to com.thedaydreamer.orbit, app name to 'Orbit revolvingAround you', and apply Orbit Mascot launcher icon

--- finance_app/android/app/build.gradle.kts
+++ finance_app/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.finance_app"
+    namespace = "com.thedaydreamer.orbit"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,8 +22,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.finance_app"
+        // Specify unique Application ID
+        applicationId = "com.thedaydreamer.orbit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
